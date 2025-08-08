0dcc26ecf2956dbf516b8b6c1e8472f3d6bafdbf
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-09 00:16:56 +0530

    Final commit

--- app/src/main/java/com/example/rockpaperscissors/ui/theme/RockPaperScissors.kt
+++ app/src/main/java/com/example/rockpaperscissors/ui/theme/RockPaperScissors.kt
@@ -4,50 +4,55 @@ fun main(){
 
     var computerChoice =""
     var playerChoice = ""
-    println("Rock, Paper or Scissors? Enter your choice:")
-    playerChoice = readln()
 
-    val randomNumber = (1..3 ).random()
+    while (true){
+        val randomNumber = (1..3 ).random()
 
-    computerChoice = when(randomNumber){
-        1 -> "Rock"
-        2 -> "Paper"
-        else -> "Scissors"
-    }
+        computerChoice = when(randomNumber){
+            1 -> "Rock"
+            2 -> "Paper"
+            else -> "Scissors"
+        }
+        println("Rock, Paper or Scissors? Enter your choice:")
+        playerChoice = readln()
 
-    if (playerChoice==computerChoice){
-        println("This is a draw")
-    }
-    else if (playerChoice == "Scissors"){
-        if(computerChoice == "Rock"){
-            println("Computer won the game")
-            readln()
+        println("Computer's choice is $computerChoice")
+
+        if (playerChoice.lowercase()==computerChoice.lowercase()){
+            println("This is a draw")
+        }
+        else if (playerChoice == "Scissors" || playerChoice == "scissors"){
+            if(computerChoice == "Rock"){
+                println("Computer won the game")
+
+            }
+            else {
+                println("You won the game")
+
+            }
+        }
+        else if (playerChoice == "Rock" || playerChoice == "rock"){
+            if(computerChoice == "Paper"){
+                println("Computer won the game")
+
+            }
+            else {
+                println("You won the game")
+
+            }
         }
         else {
-            println("You won the game")
-            readln()
+            if(computerChoice == "Scissors"){
+                println("Computer won the game")
+
+            }
+            else {
+                println("You won the game")
+
+            }
         }
     }
-    else if (playerChoice == "Rock"){
-        if(computerChoice == "Paper"){
-            println("Computer won the game")
-            readln()
-        }
-        else {
-            println("You won the game")
-            readln()
-        }
-    }
-    else {
-        if(computerChoice == "Scissors"){
-            println("Computer won the game")
-            readln()
-        }
-        else {
-            println("You won the game")
-            readln()
-        }
-    }
+
 
 
 
