0a5d0556d9206f2614dc0b62c799da1930adf06a
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-08 23:49:51 +0530

    first commit

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -4,4 +4,8 @@ fun main(){
 
     var computerChoice =""
     var playerChoice = ""
+    println("Rock, Paper or Scissors? Enter your choice:")
+    playerChoice = readln()
+
+    val randomNumber = (1..3 ).random()
 }
