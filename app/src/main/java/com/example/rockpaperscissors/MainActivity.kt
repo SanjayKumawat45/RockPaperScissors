bdfbf8034119b10623cce7b74a72ee3b3639dcf0
SanjayKumawat45 <sanjay4kumawat@gmail.com>
2025-08-08 23:57:28 +0530

    commit

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.kt
@@ -8,4 +8,27 @@ fun main(){
     playerChoice = readln()
 
     val randomNumber = (1..3 ).random()
+
+    computerChoice = when(randomNumber){
+        1 -> "Rock"
+        2 -> "Paper"
+        else -> "Scissors"
+    }
+
+    if (playerChoice==computerChoice){
+        println("This is a draw")
+    }
+    else if (playerChoice == "Scissors"){
+        if(computerChoice == "Rock"){
+            println("Computer won the game")
+            readln()
+        }
+        else {
+            println("You won the game")
+            readln()
+        }
+    }
+
+
+
 }
